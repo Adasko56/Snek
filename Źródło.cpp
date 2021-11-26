6f5780d5ab69ec4a829c9b00c40c9bf44c8af135
Kejran <kubasio221@wp.pl>
2021-11-26 17:54:27 +0100

    taktak

--- Źródło.cpp
+++ Źródło.cpp
@@ -71,12 +71,14 @@ Fruit5:
 	score = 0;
 }
 
-void calculateScore(int fruit){
-	if(fruitsEaten<10){
+void calculateScore(int fruit) {
+	if (fruitsEaten < 10) {
 		score++;
-	} else if(fruitsEaten > 10 && fruitsEaten < 15){
+	}
+	else if (fruitsEaten > 10 && fruitsEaten < 15) {
 		score -= fruit;
-	} else(){
+	}
+	else {
 		score += fruit;
 	}
 }
@@ -260,12 +262,12 @@ void Logic() {
 	for (int i = 0; i < score; i++)
 		if (tailX[i] == x && tailY[i] == y)
 			gameOver = true;
-	if (x == fruitX1 && y == fruitY1) {}
+	if (x == fruitX1 && y == fruitY1) {
 		calculateScore(fruit1);
 	loop1:	//sprawdza czy jablko nie pojawia sie w wezu
 		fruitX1 = rand() % width;
 		fruitY1 = rand() % height;
-		fruit1 = rand() % 10+0;
+		fruit1 = rand() % 10 + 0;
 		int i = 0;
 		while (i <= score) {
 			if ((fruitX1 == x && fruitY1 == y) || (fruitX1 == fruitX5 && fruitY1 == fruitY5) || (fruitX1 == fruitX2 && fruitY1 == fruitY2) || (fruitX1 == fruitX3 && fruitY1 == fruitY3) || (fruitX1 == fruitX4 && fruitY1 == fruitY4) || (fruitX1 == tailX[i] && fruitY1 == tailY[i]))
@@ -276,11 +278,11 @@ void Logic() {
 	}
 	if (x == fruitX2 && y == fruitY2) {
 		calculateScore(fruit2);
-		
+
 	loop2:	//sprawdza czy jablko nie pojawia sie w wezu
 		fruitX2 = rand() % width;
 		fruitY2 = rand() % height;
-		fruit2 = rand() % 10+0;
+		fruit2 = rand() % 10 + 0;
 		int i = 0;
 		while (i <= score) {
 			if ((fruitX2 == x && fruitY2 == y) || (fruitX1 == fruitX5 && fruitY2 == fruitY1) || (fruitX2 == fruitX5 && fruitY2 == fruitY5) || (fruitX2 == fruitX3 && fruitY2 == fruitY3) || (fruitX2 == fruitX4 && fruitY2 == fruitY4) || (fruitX2 == tailX[i] && fruitY2 == tailY[i]))
@@ -295,7 +297,7 @@ void Logic() {
 	loop3:	//sprawdza czy jablko nie pojawia sie w wezu
 		fruitX3 = rand() % width;
 		fruitY3 = rand() % height;
-		fruit3 = rand() % 10+0;
+		fruit3 = rand() % 10 + 0;
 		int i = 0;
 		while (i <= score) {
 			if ((fruitX3 == x && fruitY3 == y) || (fruitX3 == fruitX1 && fruitY3 == fruitY1) || (fruitX3 == fruitX2 && fruitY3 == fruitY2) || (fruitX3 == fruitX5 && fruitY3 == fruitY5) || (fruitX3 == fruitX4 && fruitY3 == fruitY4) || (fruitX3 == tailX[i] && fruitY3 == tailY[i]))
@@ -303,14 +305,14 @@ void Logic() {
 			else
 				i++;
 		}
-		
+
 	}
 	if (x == fruitX4 && y == fruitY4) {
 		calculateScore(fruit4);
 	loop4:	//sprawdza czy jablko nie pojawia sie w wezu
 		fruitX4 = rand() % width;
 		fruitY4 = rand() % height;
-		fruit4 = rand() % 10+0;
+		fruit4 = rand() % 10 + 0;
 		int i = 0;
 		while (i <= score) {
 			if ((fruitX4 == x && fruitY4 == y) || (fruitX4 == fruitX5 && fruitY4 == fruitY5) || (fruitX4 == fruitX2 && fruitY4 == fruitY2) || (fruitX4 == fruitX3 && fruitY4 == fruitY3) || (fruitX4 == fruitX5 && fruitY4 == fruitY5) || (fruitX4 == tailX[i] && fruitY4 == tailY[i]))
@@ -324,7 +326,7 @@ void Logic() {
 	loop5:	//sprawdza czy jablko nie pojawia sie w wezu
 		fruitX5 = rand() % width;
 		fruitY5 = rand() % height;
-		fruit5 = rand() % 10+0;
+		fruit5 = rand() % 10 + 0;
 		int i = 0;
 		while (i <= score) {
 			if ((fruitX5 == x && fruitY5 == y) || (fruitX5 == fruitX5 && fruitY5 == fruitY1) || (fruitX5 == fruitX2 && fruitY5 == fruitY2) || (fruitX5 == fruitX3 && fruitY5 == fruitY3) || (fruitX5 == fruitX4 && fruitY5 == fruitY4) || (fruitX5 == tailX[i] && fruitY5 == tailY[i]))
